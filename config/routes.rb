c101d6144dd59b54a667e42878399c927e9d02e3
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-08-21 13:35:40 +0200

    working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'articles', to: 'articles#index'
+
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+
+  # NB: The `show` route needs to be *after* `new` route.
+  get 'articles/:id', to: 'articles#show'
+
+  get 'articles/:id/edit', to: 'articles#edit'
+  patch 'articles/:id', to: 'articles#update'
+
+resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "creating articles"
+
+10.times do |article|
+  article = Article.new(
+    title: Faker::Ancient.god,
+    content: Faker::Cannabis.health_benefit
+    )
+  article.save
+
+  end
+  puts "finish"
